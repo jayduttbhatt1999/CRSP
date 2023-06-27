c40482fe3d7315ec78e02b9b1fb50439a4c777d8
shahshipra90 <shahshipr90@gmail.com>
2023-06-27 10:24:55 -0400

    final commit of this branch before merging

--- accounts/migrations/0035_alter_post_allow_downloading_alter_post_paper.py
+++ accounts/migrations/0035_alter_post_allow_downloading_alter_post_paper.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0034_post_keywords'),
+        ('accounts', '0024_post_keywords'),
     ]
 
     operations = [
